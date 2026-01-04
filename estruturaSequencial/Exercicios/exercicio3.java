f38bd55fdc30f9f1e006a6a4e79a68d37d738f25
vinip000 <112911506+vinip000@users.noreply.github.com>
2026-01-04 13:17:54 -0300

    Add files via upload

---
+++ estruturaSequencial/Exercicios/exercicio3.java
@@ -0,0 +1,23 @@
+package estruturaSequencial.Exercicios;
+
+public class exercicio3 {
+    public static void main(String[] args) {
+        System.out.println("Exercício 3 - Estrutura Sequencial");
+
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        int d = 8;
+        int diferenca = (a * b) - (c * d);
+
+        System.out.println(diferenca);
+
+
+        int x = 5;
+        int y = 6;
+        int w = -7;
+        int z = 8;
+        int diferenca2 = (x * y) - (w * z);
+        System.out.println(diferenca2);
+    }
+}
