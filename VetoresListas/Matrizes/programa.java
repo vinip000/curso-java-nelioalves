6cc86a740ed97522ed148dd7efea6fd400774e19
vinip000 <112911506+vinip000@users.noreply.github.com>
2026-02-07 10:21:53 -0300

    Add files via upload

---
+++ VetoresListas/Matrizes/programa.java
@@ -0,0 +1,39 @@
+package VetoresListas.Matrizes;
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class programa {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] mat = new int[n][n];
+
+        for (int i = 0; i < mat.length; i++){
+            for (int j = 0; j < mat[i].length; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+        
+        System.out.println("DIAGONAL PRINCIPAL:");
+        for (int i = 0; i < mat.length; i++) {
+            System.out.print(mat[i][i] + " ");
+        }
+        System.out.println();
+
+        int count = 0;
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                if (mat[i][j] < 0) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println("NÚMEROS NEGATIVOS: " + count);
+
+        sc.close();
+    }
+}
